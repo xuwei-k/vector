00612d5cd88f885687b589609b4f32a4a42d832c
xuwei-k <6b656e6a69@gmail.com>
2012-06-26 01:41:22 +0900

    fix bug

--- src/main/java/com/github/xuwei_k/VectorPointer.java
+++ src/main/java/com/github/xuwei_k/VectorPointer.java
@@ -11,11 +11,11 @@ package com.github.xuwei_k;
 public class VectorPointer<T> {
     protected int depth;
     protected Object[] display0;
-    protected Object[][] display1;
-    protected Object[][][] display2;
-    protected Object[][][][] display3;
-    protected Object[][][][][] display4;
-    protected Object[][][][][][] display5;
+    protected Object[] display1;
+    protected Object[] display2;
+    protected Object[] display3;
+    protected Object[] display4;
+    protected Object[] display5;
 
     public VectorPointer() {
     }
@@ -67,17 +67,17 @@ public class VectorPointer<T> {
 
     protected final T getElem(final int index, final int xor) {
         if (xor < (1 << 5)) { // level = 0
-            return (T) display0[index & 31];
+            return (T)display0[index & 31];
         } else if (xor < (1 << 10)) { // level = 1
-            return (T) (display1[(index >> 5) & 31][index & 31]);
+            return (T)(((Object[])display1[(index >> 5) & 31])[index & 31]);
         } else if (xor < (1 << 15)) { // level = 2
-            return (T) (display2[(index >> 10) & 31][(index >> 5) & 31][index & 31]);
+            return (T)(((Object[])(((Object[])display2[(index >> 10) & 31])[(index >> 5) & 31]))[index & 31]);
         } else if (xor < (1 << 20)) { // level = 3
-            return (T) (display3[(index >> 15) & 31][(index >> 10) & 31][(index >> 5) & 31][index & 31]);
+            return (T)(((Object[])((Object[])((Object[])display3[(index >> 15) & 31])[(index >> 10) & 31])[(index >> 5) & 31])[index & 31]);
         } else if (xor < (1 << 25)) { // level = 4
-            return (T) (display4[(index >> 20) & 31][(index >> 15) & 31][(index >> 10) & 31][(index >> 5) & 31][index & 31]);
+            return (T)(((Object[])((Object[])((Object[])((Object[])display4[(index >> 20) & 31])[(index >> 15) & 31])[(index >> 10) & 31])[(index >> 5) & 31])[index & 31]);
         } else if (xor < (1 << 30)) { // level = 5
-            return (T) (display5[(index >> 25) & 31][(index >> 20) & 31][(index >> 15) & 31][(index >> 10) & 31][(index >> 5) & 31][index & 31]);
+            return (T)(((Object[])((Object[])((Object[])((Object[])((Object[])display5[(index >> 25) & 31])[(index >> 20) & 31])[(index >> 15) & 31])[(index >> 10) & 31])[(index >> 5) & 31])[index & 31]);
         } else { // level = 6
             throw new IllegalArgumentException();
         }
@@ -86,25 +86,25 @@ public class VectorPointer<T> {
     protected final void gotoPos(final int index, final int xor) {
         if (xor < (1 << 5)) { // level = 0 (could maybe removed)
         } else if (xor < (1 << 10)) { // level = 1
-            display0 = display1[(index >> 5) & 31];
+            display0 = (Object[])display1[(index >> 5) & 31];
         } else if (xor < (1 << 15)) { // level = 2
-            display1 = display2[(index >> 10) & 31];
-            display0 = display1[(index >> 5) & 31];
+            display1 = (Object[])display2[(index >> 10) & 31];
+            display0 = (Object[])display1[(index >> 5) & 31];
         } else if (xor < (1 << 20)) { // level = 3
-            display2 = display3[(index >> 15) & 31];
-            display1 = display2[(index >> 10) & 31];
-            display0 = display1[(index >> 5) & 31];
+            display2 = (Object[])display3[(index >> 15) & 31];
+            display1 = (Object[])display2[(index >> 10) & 31];
+            display0 = (Object[])display1[(index >> 5) & 31];
         } else if (xor < (1 << 25)) { // level = 4
-            display3 = display4[(index >> 20) & 31];
-            display2 = display3[(index >> 15) & 31];
-            display1 = display2[(index >> 10) & 31];
-            display0 = display1[(index >> 5) & 31];
+            display3 = (Object[])display4[(index >> 20) & 31];
+            display2 = (Object[])display3[(index >> 15) & 31];
+            display1 = (Object[])display2[(index >> 10) & 31];
+            display0 = (Object[])display1[(index >> 5) & 31];
         } else if (xor < (1 << 30)) { // level = 5
-            display4 = display5[(index >> 25) & 31];
-            display3 = display4[(index >> 20) & 31];
-            display2 = display3[(index >> 15) & 31];
-            display1 = display2[(index >> 10) & 31];
-            display0 = display1[(index >> 5) & 31];
+            display4 = (Object[])display5[(index >> 25) & 31];
+            display3 = (Object[])display4[(index >> 20) & 31];
+            display2 = (Object[])display3[(index >> 15) & 31];
+            display1 = (Object[])display2[(index >> 10) & 31];
+            display0 = (Object[])display1[(index >> 5) & 31];
         } else { // level = 6
             throw new IllegalArgumentException();
         }
@@ -112,25 +112,25 @@ public class VectorPointer<T> {
 
     protected final void gotoNextBlockStart(final int index, final int xor) {
         if (xor < (1 << 10)) { // level = 1
-            display0 = display1[(index >> 5) & 31];
+            display0 = (Object[])display1[(index >> 5) & 31];
         } else if (xor < (1 << 15)) { // level = 2
-            display1 = display2[(index >> 10) & 31];
-            display0 = display1[0];
+            display1 = (Object[])display2[(index >> 10) & 31];
+            display0 = (Object[])display1[0];
         } else if (xor < (1 << 20)) { // level = 3
-            display2 = display3[(index >> 15) & 31];
-            display1 = display2[0];
-            display0 = display1[0];
+            display2 = (Object[])display3[(index >> 15) & 31];
+            display1 = (Object[])display2[0];
+            display0 = (Object[])display1[0];
         } else if (xor < (1 << 25)) { // level = 4
-            display3 = display4[(index >> 20) & 31];
-            display2 = display3[0];
-            display1 = display2[0];
-            display0 = display1[0];
+            display3 = (Object[])display4[(index >> 20) & 31];
+            display2 = (Object[])display3[0];
+            display1 = (Object[])display2[0];
+            display0 = (Object[])display1[0];
         } else if (xor < (1 << 30)) { // level = 5
-            display4 = display5[(index >> 25) & 31];
-            display3 = display4[0];
-            display2 = display3[0];
-            display1 = display2[0];
-            display0 = display1[0];
+            display4 = (Object[])display5[(index >> 25) & 31];
+            display3 = (Object[])display4[0];
+            display2 = (Object[])display3[0];
+            display1 = (Object[])display2[0];
+            display0 = (Object[])display1[0];
         } else { // level = 6
             throw new IllegalArgumentException();
         }
@@ -359,77 +359,77 @@ public class VectorPointer<T> {
             // println("XXX clean with low xor")
         } else if (xor < (1 << 10)) { // level = 1
             if (depth == 1) {
-                display1 = (Object[][]) new Object[32];
+                display1 = new Object[32];
                 display1[(oldIndex >> 5) & 31] = display0;
                 depth++;
             }
             display0 = new Object[32];
         } else if (xor < (1 << 15)) { // level = 2
             if (depth == 2) {
-                display2 = (Object[][][]) new Object[32];
+                display2 = new Object[32];
                 display2[(oldIndex >> 10) & 31] = display1;
                 depth++;
             }
-            display1 = display2[(newIndex >> 10) & 31];
+            display1 = (Object[])display2[(newIndex >> 10) & 31];
             if (display1 == null)
-                display1 = (Object[][]) new Object[32];
+                display1 = new Object[32];
             display0 = new Object[32];
         } else if (xor < (1 << 20)) { // level = 3
             if (depth == 3) {
-                display3 = (Object[][][][]) new Object[32];
+                display3 = new Object[32];
                 display3[(oldIndex >> 15) & 31] = display2;
-                display2 = (Object[][][]) new Object[32];
-                display1 = (Object[][]) new Object[32];
+                display2 = new Object[32];
+                display1 = new Object[32];
                 depth++;
             }
-            display2 = display3[(newIndex >> 15) & 31];
+            display2 = (Object[])display3[(newIndex >> 15) & 31];
             if (display2 == null)
-                display2 = (Object[][][]) new Object[32];
-            display1 = display2[(newIndex >> 10) & 31];
+                display2 = new Object[32];
+            display1 = (Object[])display2[(newIndex >> 10) & 31];
             if (display1 == null)
-                display1 = (Object[][]) new Object[32];
+                display1 = new Object[32];
             display0 = new Object[32];
         } else if (xor < (1 << 25)) { // level = 4
             if (depth == 4) {
-                display4 = (Object[][][][][]) new Object[32];
+                display4 = new Object[32];
                 display4[(oldIndex >> 20) & 31] = display3;
-                display3 = (Object[][][][]) new Object[32];
-                display2 = (Object[][][]) new Object[32];
-                display1 = (Object[][]) new Object[32];
+                display3 = new Object[32];
+                display2 = new Object[32];
+                display1 = new Object[32];
                 depth++;
             }
-            display3 = display4[(newIndex >> 20) & 31];
+            display3 = (Object[])display4[(newIndex >> 20) & 31];
             if (display3 == null)
-                display3 = (Object[][][][]) new Object[32];
-            display2 = display3[(newIndex >> 15) & 31];
+                display3 = new Object[32];
+            display2 = (Object[])display3[(newIndex >> 15) & 31];
             if (display2 == null)
-                display2 = (Object[][][]) new Object[32];
-            display1 = display2[(newIndex >> 10) & 31];
+                display2 = new Object[32];
+            display1 = (Object[])display2[(newIndex >> 10) & 31];
             if (display1 == null)
-                display1 = (Object[][]) new Object[32];
+                display1 = new Object[32];
             display0 = new Object[32];
         } else if (xor < (1 << 30)) { // level = 5
             if (depth == 5) {
-                display5 = (Object[][][][][][]) new Object[32];
+                display5 = new Object[32];
                 display5[(oldIndex >> 25) & 31] = display4;
-                display4 = (Object[][][][][]) new Object[32];
-                display3 = (Object[][][][]) new Object[32];
-                display2 = (Object[][][]) new Object[32];
-                display1 = (Object[][]) new Object[32];
+                display4 = new Object[32];
+                display3 = new Object[32];
+                display2 = new Object[32];
+                display1 = new Object[32];
                 depth += 1;
             }
-            display4 = display5[(newIndex >> 20) & 31];
+            display4 = (Object[])display5[(newIndex >> 20) & 31];
             if (display4 == null)
-                display4 = (Object[][][][][]) new Object[32];
-            display3 = display4[(newIndex >> 20) & 31];
+                display4 = new Object[32];
+            display3 = (Object[])display4[(newIndex >> 20) & 31];
             if (display3 == null)
-                display3 = (Object[][][][]) new Object[32];
-            display2 = display3[(newIndex >> 15) & 31];
+                display3 = new Object[32];
+            display2 = (Object[])display3[(newIndex >> 15) & 31];
             if (display2 == null)
-                display2 = (Object[][][]) new Object[32];
-            display1 = display2[(newIndex >> 10) & 31];
+                display2 = new Object[32];
+            display1 = (Object[])display2[(newIndex >> 10) & 31];
             if (display1 == null)
-                display1 = (Object[][]) new Object[32];
+                display1 = new Object[32];
             display0 = new Object[32];
         } else { // level = 6
             throw new IllegalArgumentException();
